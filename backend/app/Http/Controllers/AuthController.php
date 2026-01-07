c8001b07f6ccbd1137801d33f808dcf1172662f9
DevWisdom08 <arminhardy567@gmail.com>
2026-01-07 13:49:48 +0900

    fix: added missing Log facade import in AuthController

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 // use Tymon\JWTAuth\Facades\JWTAuth;
 
